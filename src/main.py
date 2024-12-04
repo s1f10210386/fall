7b7a97fdf10dc59e0676d6272d81a289aa2766ef
hotaka <s1f102103864@iniad.org>
2024-12-05 02:25:08 +0900

    fix: multiple -> singlepose

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ import tensorflow_hub as hub
 KEYPOINT_THRESHOLD = 0.2
 
 def main():
-    # Tensorflow Hubを利用してモデルダウンロード
-    model = hub.load('https://tfhub.dev/google/movenet/multipose/lightning/1')
+    # TensorFlow Hubから単一人物用のMoveNetモデルをダウンロード
+    model = hub.load('https://tfhub.dev/google/movenet/singlepose/lightning/4')
     movenet = model.signatures['serving_default']
     
     # カメラ設定
@@ -21,10 +21,10 @@ def main():
             break
 
         # 推論実行
-        keypoints_list, scores_list, bbox_list = run_inference(movenet, frame)
+        keypoints, scores = run_inference(movenet, frame)
 
         # 画像レンダリング
-        result_image = render(frame, keypoints_list, scores_list, bbox_list)
+        result_image = render(frame, keypoints, scores)
 
         cv2.namedWindow("image", cv2.WINDOW_FULLSCREEN)
         cv2.imshow('image', result_image)
@@ -37,69 +37,49 @@ def main():
 
 def run_inference(model, image):
     # 画像の前処理
-    input_image = cv2.resize(image, dsize=(256, 256))
+    input_image = cv2.resize(image, dsize=(192, 192))
     input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
-    input_image = np.expand_dims(input_image, 0)
+    input_image = np.expand_dims(input_image, axis=0)
     input_image = tf.cast(input_image, dtype=tf.int32)
-
+    
     # 推論実行・結果取得
     outputs = model(input_image)
-    keypoints = np.squeeze(outputs['output_0'].numpy())
-
+    keypoints_with_scores = outputs['output_0'].numpy()
+    
+    # 結果の解析
+    keypoints_with_scores = np.squeeze(keypoints_with_scores)
+    
+    keypoints = keypoints_with_scores[:, :2]
+    scores = keypoints_with_scores[:, 2]
+    
+    # キーポイントを画像座標に変換
     image_height, image_width = image.shape[:2]
-    keypoints_list, scores_list, bbox_list = [], [], []
+    keypoints[:, 0] *= image_height  # y座標
+    keypoints[:, 1] *= image_width   # x座標
+    keypoints = keypoints.astype(int)
+    
+    return keypoints, scores
 
-    # 検出した人物ごとにキーポイントのフォーマット処理
-    for kp in keypoints:
-        keypoints = []
-        scores = []
-        for index in range(17):
-            kp_x = int(image_width * kp[index*3+1])
-            kp_y = int(image_height * kp[index*3+0])
-            score = kp[index*3+2]
-            keypoints.append([kp_x, kp_y])
-            scores.append(score)
-        bbox_ymin = int(image_height * kp[51])
-        bbox_xmin = int(image_width * kp[52])
-        bbox_ymax = int(image_height * kp[53])
-        bbox_xmax = int(image_width * kp[54])
-        bbox_score = kp[55]
-
-        keypoints_list.append(keypoints)
-        scores_list.append(scores)
-        bbox_list.append([bbox_xmin, bbox_ymin, bbox_xmax, bbox_ymax, bbox_score])
-
-    return keypoints_list, scores_list, bbox_list
-
-def render(image, keypoints_list, scores_list, bbox_list):
+def render(image, keypoints, scores):
     render = image.copy()
-    for i, (keypoints, scores, bbox) in enumerate(zip(keypoints_list, scores_list, bbox_list)):
-        if bbox[4] < 0.2:
-            continue
-
-        cv2.rectangle(render, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 2)
-
-        # 0:nose, 1:left eye, 2:right eye, 3:left ear, 4:right ear, 5:left shoulder, 6:right shoulder, 7:left elbow, 8:right elbow, 9:left wrist, 10:right wrist,
-        # 11:left hip, 12:right hip, 13:left knee, 14:right knee, 15:left ankle, 16:right ankle
-        # 接続するキーポイントの組
-        kp_links = [
-            (0,1),(0,2),(1,3),(2,4),(0,5),(0,6),(5,6),(5,7),(7,9),(6,8),
-            (8,10),(11,12),(5,11),(11,13),(13,15),(6,12),(12,14),(14,16)
-        ]
-        for kp_idx_1, kp_idx_2 in kp_links:
-            kp_1 = keypoints[kp_idx_1]
-            kp_2 = keypoints[kp_idx_2]
-            score_1 = scores[kp_idx_1]
-            score_2 = scores[kp_idx_2]
-            if score_1 > KEYPOINT_THRESHOLD and score_2 > KEYPOINT_THRESHOLD:
-                cv2.line(render, tuple(kp_1), tuple(kp_2), (0,0,255), 2)
-
-        for idx, (keypoint, score) in enumerate(zip(keypoints, scores)):
-            if score > KEYPOINT_THRESHOLD:
-                cv2.circle(render, tuple(keypoint), 4, (0,0,255), -1)
-
+    # 接続するキーポイントの組
+    kp_links = [
+        (0,1),(0,2),(1,3),(2,4),(0,5),(0,6),(5,6),(5,7),(7,9),(6,8),
+        (8,10),(11,12),(5,11),(11,13),(13,15),(6,12),(12,14),(14,16)
+    ]
+    for kp_idx_1, kp_idx_2 in kp_links:
+        kp_1 = keypoints[kp_idx_1]
+        kp_2 = keypoints[kp_idx_2]
+        score_1 = scores[kp_idx_1]
+        score_2 = scores[kp_idx_2]
+        if score_1 > KEYPOINT_THRESHOLD and score_2 > KEYPOINT_THRESHOLD:
+            cv2.line(render, (kp_1[1], kp_1[0]), (kp_2[1], kp_2[0]), (0,0,255), 2)
+    
+    for idx, (keypoint, score) in enumerate(zip(keypoints, scores)):
+        if score > KEYPOINT_THRESHOLD:
+            cv2.circle(render, (keypoint[1], keypoint[0]), 4, (0,0,255), -1)
+    
     return render
 
 if __name__ == '__main__':
     main()
-
